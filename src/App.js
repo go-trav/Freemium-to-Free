e82d36c333c37c224e1734d45d707a51aa94045d
mohithworks <mohithworks2002@gmail.com>
2022-12-27 21:52:30 +0530

    mohith

--- src/App.js
+++ src/App.js
@@ -165,7 +165,7 @@ export default function App() {
           <Sidenav
             color={sidenavColor}
             brand={brand}
-            brandName="Ad Platform"
+            brandName="AD PLATFORM"
             routes={routes}
             onMouseEnter={handleOnMouseEnter}
             onMouseLeave={handleOnMouseLeave}
